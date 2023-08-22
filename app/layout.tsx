3f1ca07028e007577d730495fffe890a29155b6a
abubakar471 <abdurjoy2001@gmail.com>
2023-08-22 10:00:34 +0600

    ready for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Metaphor',
   description: 'saas ai',
+  icons : {
+    icon : "/favicon.ico"
+  }
 }
 
 export default function RootLayout({
